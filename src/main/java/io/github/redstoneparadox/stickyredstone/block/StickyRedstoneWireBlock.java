689c38df9ce6999ec68d37efdf4200d4862667f7
RedstoneParadox <spinjitzu00@gmail.com>
2023-06-03 20:09:31 -0700

    StickyRedstoneWireBlock extends Block
    Don't need everything in RedstoneWireBlock

--- src/main/java/io/github/redstoneparadox/stickyredstone/block/StickyRedstoneWireBlock.java
+++ src/main/java/io/github/redstoneparadox/stickyredstone/block/StickyRedstoneWireBlock.java
@@ -1,8 +1,9 @@
 package io.github.redstoneparadox.stickyredstone.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.RedstoneWireBlock;
 
-public class StickyRedstoneWireBlock extends RedstoneWireBlock {
+public class StickyRedstoneWireBlock extends Block {
 	public StickyRedstoneWireBlock(Settings settings) {
 		super(settings);
 	}
